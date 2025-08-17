25559885ef234bb73e3d6474ff69863fa6c3701d
Kante Sreenithin <71339114+kantesreenithin@users.noreply.github.com>
2025-08-17 16:40:51 +0530

    Update contants.js

--- frontend/notes-app/src/utils/contants.js
+++ frontend/notes-app/src/utils/contants.js
@@ -1,3 +1,4 @@
-export const BASE_URL = "https://notes-mdo9.onrender.com";
+export const BASE_URL = "https://notes-backend-iota-wine.vercel.app"
+  // "https://notes-mdo9.onrender.com";
 // "http://localhost:8000";
 
